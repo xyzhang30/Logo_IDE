4e76932c9933500cf00dcd89858e8a8e005f6296
Kevin Deng <kd254@duke.edu>
2024-02-17 20:23:40 -0500

    commandParser comments

--- doc/plan/api/command/commandParserExternal.java
+++ doc/plan/api/command/commandParserExternal.java
@@ -1,8 +1,27 @@
 package command;
 
 public interface commandParser {
+
+  /**
+   * Process input stream. Throw exceptions for bad commands/syntax. Interprets and adds commands to
+   * Stack of commands, history.
+   */
   public void parseInput(String);
+
+  /**
+   * Executes the command at the current line.
+   */
   public void executeCurrentLine();
+
+  /**
+   * Returns the index of the current line marker. Where the commandParser is at in the code.
+   * @return int  index of current line marker.
+   */
   public int getCurrentLine();
+
+  /**
+   * Sets the current line marker to the specified index. Throws an exception if the index provided
+   * is not a valid index.
+   */
   public void setCurrentLine(int);
 }
