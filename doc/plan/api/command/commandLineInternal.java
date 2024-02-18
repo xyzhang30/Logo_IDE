1670922d56d04e477b3179b1fc0a69a6e3410742
Kevin Deng <kd254@duke.edu>
2024-02-17 20:25:22 -0500

    commandLine comment

--- doc/plan/api/command/commandLineInternal.java
+++ doc/plan/api/command/commandLineInternal.java
@@ -1,5 +1,10 @@
 package command;
 
 public interface commandLine{
+
+  /**
+   * Executes the code stored on this commandLine object
+   * @return double  return value of the SLogo keyword
+   */
   public double execute();
 }
