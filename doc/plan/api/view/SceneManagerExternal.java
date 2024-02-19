2c6e90f644a4e6abadd7ba549af7bcc5ab7fa437
Spencer Katz <sek49@duke.edu>
2024-02-18 21:08:36 -0500

    API's added

--- doc/plan/api/view/SceneManagerExternal.java
+++ doc/plan/api/view/SceneManagerExternal.java
@@ -1,10 +1,20 @@
 package view;
 
-public interface SceneManager{
-
+public interface ViewInternal {
   /**
    * Returns the scene.
    * @return Group  the internally stored scene
    */
-  public Group getScene();
+  Group getScene();
+
+  setTheme();
+
+  setLanguage(String language);
+
+  setColor(String color);
+
+  makeButton(int x, int y);
+
+  switchScene();
+
 }
---
+++ doc/plan/api/view/TurtleViewInternal.java
@@ -0,0 +1,7 @@
+package view;
+
+public interface TurtleViewInternal {
+
+  void drawHere(int xDestination, int yDestination, int Orientation);
+
+}
