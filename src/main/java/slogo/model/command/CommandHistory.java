d43386e8d9b250c63c71541f8e5b3d165d25323e
Kevin Deng <kd254@duke.edu>
2024-02-21 03:35:50 -0500

    A basic History tracker class implementation. Not intended to replace AST.

---
+++ src/main/java/slogo/model/command/CommandHistory.java
@@ -0,0 +1,45 @@
+package slogo.model.command;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * A list of commandLines. Manages the history of entered commands.
+ */
+public class CommandHistory implements History{
+  private List<CommandLine> commands;
+  private int currentIndex;
+  public CommandHistory(){
+    clearHistory();
+  }
+  public void executeCurrentCommand(){
+    commands.get(currentIndex).execute();
+  }
+  public void incrementCommandIndex() {
+    if (currentIndex>commands.size()){
+      throw new IndexOutOfBoundsException(String.format(
+          "Attempted to increment past end of history. History has %s items.", commands.size()));
+    }
+    currentIndex++;
+  }
+  public void addCommand(CommandLine com){
+    commands.add(com);
+  }
+  public void jumpToCommand(int index){
+    if (index>commands.size() || index<0){
+      throw new IndexOutOfBoundsException("Attempted to jump to an invalid index in history");
+    }
+    currentIndex = index;
+  }
+  public CommandLine getCurrentCommand(){
+    return commands.get(currentIndex);
+  }
+  public int getCurrentIndex(){
+    return currentIndex;
+  }
+  private void clearHistory() {
+    commands = new ArrayList<>();
+    currentIndex = 0;
+  }
+
+}
