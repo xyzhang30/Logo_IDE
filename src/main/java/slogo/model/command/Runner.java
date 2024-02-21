c8ce2a2b070d212bcb3918e2b2af83cf9ebae637
Alisha Zhang <xz352@duke.edu>
2024-02-20 22:35:00 -0500

    DESIGN-17: added commandline interface + singlelinecommand abstract class for commands + runner for testing commands

---
+++ src/main/java/slogo/model/command/Runner.java
@@ -0,0 +1,16 @@
+package slogo.model.command;
+
+import slogo.model.turtle.TurtleModel;
+
+public class Runner {
+
+  public static void main(String[] args) {
+    TurtleModel turt = new TurtleModel();
+    turt.setDirection(135);
+    Forward forward = new Forward(50, turt);
+    forward.execute();
+    System.out.println("new x : "+ turt.getPosX());
+    System.out.println("new y: " + turt.getPoxY());
+  }
+
+}
---
+++ src/main/java/slogo/model/command/SingleLineCommand.java
@@ -0,0 +1,16 @@
+package slogo.model.command;
+
+import slogo.model.turtle.TurtleModel;
+
+public abstract class SingleLineCommand implements CommandLine {
+
+  private TurtleModel turtle;
+
+  public SingleLineCommand(TurtleModel turtle){
+    this.turtle = turtle;
+  }
+
+  public TurtleModel getTurtle(){
+    return turtle;
+  }
+}
