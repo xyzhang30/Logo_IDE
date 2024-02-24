95e89e6afc26669068b84c94505b47d84ee73ad4
Alisha Zhang <xz352@duke.edu>
2024-02-24 17:40:36 -0500

    added turtle and getters for turtle and param list

--- src/main/java/slogo/model/command/CommandExecutable.java
+++ src/main/java/slogo/model/command/CommandExecutable.java
@@ -1,10 +1,19 @@
 package slogo.model.command;
 
 import java.util.List;
+import slogo.model.api.Executable;
+import slogo.model.turtle.TurtleModel;
 
-public abstract class CommandExecutable implements Executable{
+public abstract class CommandExecutable implements Executable {
   private List<Executable> parameterExecutables;
   private List<Double> parameterData;
+  private TurtleModel turtle;
+
+  public CommandExecutable(List<Executable> parameterExecutables, TurtleModel turtle){
+    this.parameterExecutables = parameterExecutables;
+    this.turtle = turtle;
+    execute(); //?
+  }
 
   public abstract double internalLogicExecution();
 
@@ -14,5 +23,13 @@ public abstract class CommandExecutable implements Executable{
       parameterData.add(e.execute());
     }
     return 0;
+  }
+
+  protected TurtleModel getTurtle() {
+    return turtle;
+  }
+
+  protected List<Double> getParameterData() {
+    return parameterData;
   }
 }
