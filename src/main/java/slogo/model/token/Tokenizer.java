8c70a7537f3d35187f432bc3d75bb9aae61fa6d2
Alisha Zhang <xz352@duke.edu>
2024-02-25 13:45:24 -0500

    DESIGN-15: added throw exceptions in tokenizer api

--- src/main/java/slogo/model/token/Tokenizer.java
+++ src/main/java/slogo/model/token/Tokenizer.java
@@ -2,11 +2,14 @@ package slogo.model.token;
 
 import java.util.List;
 import slogo.model.api.InvalidCommandException;
+import slogo.model.api.InvalidConstantException;
+import slogo.model.api.InvalidVariableException;
+import slogo.model.api.UnterminatedListException;
 
 /**
  * Internal API for Tokenizer. Turns Strings into Tokens. For use by Parser.
  */
 public interface Tokenizer {
 
-  List<Token> tokenize(String lineInput) throws InvalidCommandException;
+  List<Token> tokenize(String lineInput) throws InvalidCommandException, InvalidConstantException, InvalidVariableException, UnterminatedListException;
 }
