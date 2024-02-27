e3c3f000e3ecaca788980254ec1e6d986953d3f7
Kevin Deng <kd254@duke.edu>
2024-02-27 17:28:28 -0500

    Preparing Parser for implementation

--- src/main/java/slogo/model/parser/TreeParser.java
+++ src/main/java/slogo/model/parser/TreeParser.java
@@ -1,20 +1,30 @@
 package slogo.model.parser;
 
+import slogo.model.api.ExecutionerApi;
 import slogo.model.api.InputRecord;
 import slogo.model.api.InvalidParameterNumberException;
 import slogo.model.api.ParserApi;
+import slogo.model.command.Executioner;
 import slogo.model.command.executables.Executable;
+import slogo.model.token.Token;
 import slogo.model.token.Tokenizer;
 import slogo.model.token.TokenizerApi;
 
 public class TreeParser implements ParserApi {
+  private ExecutionerApi root;
   private TokenizerApi tokenizer;
   public TreeParser(){
+    root = new Executioner();
     tokenizer = new Tokenizer("English");
   }
   @Override
   public Executable parseTree(InputRecord myRecord) throws InvalidParameterNumberException {
-    String base = myRecord.input();
+    String[] lines = myRecord.input().split("\n");
+    for (String line : lines){
+      for (Token t : tokenizer.tokenize(line)){
+        System.out.println(t.type());
+      }
+    }
     return null;
   }
 }
