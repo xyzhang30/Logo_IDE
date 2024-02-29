f006dc6e667fbce2d73cd0cd6c2406e8490595c8
Alisha Zhang <xz352@duke.edu>
2024-02-29 00:16:57 -0500

    SLOGO-12: added SETXY

---
+++ src/main/java/slogo/model/command/executables/Setxy.java
@@ -0,0 +1,27 @@
+package slogo.model.command.executables;
+
+import java.util.List;
+import slogo.model.turtle.TurtleModel;
+
+public class Setxy extends CommandExecutable{
+
+  private double newPosX;
+  private double newPoxY;
+
+  public Setxy(List<Executable> parameterExecutables, TurtleModel turtle) {
+    super(parameterExecutables, turtle);
+    newPosX = getParameterData().get(0);
+    newPoxY = getParameterData().get(1);
+  }
+
+  @Override
+  public double internalLogicExecution() {
+    double distMoved = Math.sqrt(Math.pow(getTurtle().getPosX() - newPosX, 2) + Math.pow(getTurtle().getPosY() - newPoxY, 2));
+
+    getTurtle().setPosX(newPosX);
+    getTurtle().setPosY(newPoxY);
+
+    return distMoved;
+  }
+
+}
