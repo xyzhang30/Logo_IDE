ed12de54513560213d84d26367641c01b304163d
Alisha Zhang <xz352@duke.edu>
2024-03-01 15:15:35 -0500

    created math test file

---
+++ src/test/java/slogo/model/MathCommandTest.java
@@ -0,0 +1,24 @@
+package slogo.model;
+
+import org.junit.jupiter.api.Test;
+import slogo.model.command.executables.mathCommand.RandomCommand;
+
+public class MathCommandTest {
+
+
+  @Test
+  void TestSum(){}
+
+  @Test
+  void TestDifference(){}
+
+  @Test
+  void TestProduct(){}
+
+  @Test
+  void TestQuotient(){}
+
+  @Test
+  void TestRandom(){}
+
+}
