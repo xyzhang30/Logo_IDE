069bd890a0596df34d059a3bebd7db6804a0d4cb
Alisha Zhang <xz352@duke.edu>
2024-03-01 22:59:48 -0500

    SLOGO-19,20,21,22,23

---
+++ src/main/java/slogo/model/command/executables/turtleCommand/Ycor.java
@@ -0,0 +1,18 @@
+package slogo.model.command.executables.turtleCommand;
+
+import java.util.List;
+import slogo.model.command.executables.Executable;
+import slogo.model.turtle.TurtleModel;
+
+public class Ycor extends TurtleExecutable{
+
+  public Ycor(List<Executable> parameterExecutables,
+      TurtleModel t) {
+    super(parameterExecutables, t);
+  }
+
+  @Override
+  public double execute() {
+    return getTurtle().getPosY();
+  }
+}
