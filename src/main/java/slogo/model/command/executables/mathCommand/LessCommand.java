72b0d24cc4f87c2df1bdbd73141e5743e49a17df
Alisha Zhang <xz352@duke.edu>
2024-03-02 21:28:26 -0500

    SLOGO-41: command class

---
+++ src/main/java/slogo/model/command/executables/mathCommand/LessCommand.java
@@ -0,0 +1,25 @@
+package slogo.model.command.executables.mathCommand;
+
+import java.util.List;
+import slogo.model.command.executables.CommandExecutable;
+import slogo.model.command.executables.Executable;
+
+public class LessCommand extends CommandExecutable {
+
+  private double test1;
+  private double test2;
+
+  public LessCommand(List<Executable> parameterExecutables) {
+    super(parameterExecutables);
+    test1 = parameterExecutables.get(0).execute();
+    test2 = parameterExecutables.get(1).execute();
+  }
+
+  @Override
+  public double execute() {
+    if ((test1 - test2) < 0.001){
+      return 1;
+    }
+    return 0;
+  }
+}
