571931a4a907ee8b691b1f775aa904e5b1114e29
Alisha Zhang <xz352@duke.edu>
2024-03-02 21:29:40 -0500

    DESIGN-18: logic command tests

--- src/test/java/slogo/model/MathCommandTest.java
+++ src/test/java/slogo/model/MathCommandTest.java
@@ -11,8 +11,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import slogo.model.command.executables.ConstantExecutable;
 import slogo.model.command.executables.Executable;
+import slogo.model.command.executables.mathCommand.AndCommand;
 import slogo.model.command.executables.mathCommand.DifferenceCommand;
+import slogo.model.command.executables.mathCommand.EqualCommand;
+import slogo.model.command.executables.mathCommand.GreaterCommand;
+import slogo.model.command.executables.mathCommand.LessCommand;
 import slogo.model.command.executables.mathCommand.MinusCommand;
+import slogo.model.command.executables.mathCommand.OrCommand;
 import slogo.model.command.executables.mathCommand.ProductCommand;
 import slogo.model.command.executables.mathCommand.QuotientCommand;
 import slogo.model.command.executables.mathCommand.RandomCommand;
@@ -72,6 +77,62 @@ public class MathCommandTest {
     params.add(new ConstantExecutable(50));
     MinusCommand minusCommand = new MinusCommand(params);
     assertEquals(-50, minusCommand.execute());
+  }
+
+  @Test
+  void TestAnd(){
+    params.add(new ConstantExecutable(1));
+    params.add(new ConstantExecutable(2));
+    AndCommand andCommand = new AndCommand(params);
+    assertEquals(1, andCommand.execute());
+  }
+
+  @Test
+  void TestEqualSameNumber(){
+    params.add(new ConstantExecutable(2));
+    params.add(new ConstantExecutable(2));
+    EqualCommand equalCommand = new EqualCommand(params);
+    assertEquals(1, equalCommand.execute());
+  }
+
+  @Test
+  void TestEqualErrorTolerance(){
+    params.add(new ConstantExecutable(2.00001));
+    params.add(new ConstantExecutable(2));
+    EqualCommand equalCommand = new EqualCommand(params);
+    assertEquals(1, equalCommand.execute());
+  }
+
+  @Test
+  void TestNotEqual(){
+    params.add(new ConstantExecutable(2));
+    params.add(new ConstantExecutable(2.01));
+    EqualCommand equalCommand = new EqualCommand(params);
+    assertEquals(0, equalCommand.execute());
+  }
+
+  @Test
+  void TestGreater(){
+    params.add(new ConstantExecutable(2));
+    params.add(new ConstantExecutable(2));
+    GreaterCommand greaterCommand = new GreaterCommand(params);
+    assertEquals(0, greaterCommand.execute());
+  }
+
+  @Test
+  void TestLess(){
+    params.add(new ConstantExecutable(1));
+    params.add(new ConstantExecutable(2));
+    LessCommand lessCommand = new LessCommand(params);
+    assertEquals(1, lessCommand.execute());
+  }
+
+  @Test
+  void TestOr(){
+    params.add(new ConstantExecutable(1));
+    params.add(new ConstantExecutable(0));
+    OrCommand orCommand = new OrCommand(params);
+    assertEquals(1, orCommand.execute());
   }
 
 }
