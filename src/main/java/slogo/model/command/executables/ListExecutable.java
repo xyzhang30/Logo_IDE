8b41c5118af113906e31e9f3a94d56d07c09b0be
Kevin Deng <kd254@duke.edu>
2024-03-02 22:38:00 -0500

    SLOGO-04 List Executable redesigned to use internal indices for iteration

--- src/main/java/slogo/model/command/executables/ListExecutable.java
+++ src/main/java/slogo/model/command/executables/ListExecutable.java
@@ -3,19 +3,47 @@ package slogo.model.command.executables;
 import java.util.List;
 
 public class ListExecutable implements Executable {
-  private List<Executable> myList;
-  public ListExecutable(List<Executable> tlist){
+
+  private final List<Executable> myList;
+  private int currentIndex;
+
+  public ListExecutable(List<Executable> tlist) {
     myList = tlist;
+    currentIndex = 0;
   }
-  public List<Executable> getList(){
+
+  public List<Executable> getList() {
     return myList;
   }
+
   /**
-   * Should not ever be called!
+   * Returns execution of the Executable at the current list index
+   *
    * @return 0  Nothing, this is just a wrapped list
    */
   @Override
   public double execute() {
-    return 0;
+    return myList.get(currentIndex).execute();
+  }
+
+  /**
+   * Increments current index of list.
+   */
+  public void nextIndex() {
+    currentIndex += 1;
+  }
+
+  /**
+   * Sets current index of list back to 0.
+   */
+  public void resetIndex() {
+    currentIndex = 0;
+  }
+
+  /**
+   * Executes all Executables in this list at once.
+   */
+  public void executeAll() {
+    myList.forEach(Executable::execute);
   }
 }
