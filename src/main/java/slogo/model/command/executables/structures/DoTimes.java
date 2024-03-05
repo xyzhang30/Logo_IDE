c46a08b8df683cd71bc2e44d733843dccc6f9a51
Alisha Zhang <xz352@duke.edu>
2024-03-05 15:31:21 -0500

    SLOGO-51: dotimes command class

---
+++ src/main/java/slogo/model/command/executables/structures/DoTimes.java
@@ -0,0 +1,36 @@
+package slogo.model.command.executables.structures;
+
+import java.util.List;
+import slogo.model.command.executables.CommandExecutable;
+import slogo.model.command.executables.ConstantExecutable;
+import slogo.model.command.executables.Executable;
+import slogo.model.command.executables.ListExecutable;
+import slogo.model.command.executables.VariableExecutable;
+import slogo.model.environment.EnvironmentApi;
+
+public class DoTimes extends CommandExecutable {
+
+  private VariableExecutable var;
+  private ConstantExecutable limit;
+  private final ListExecutable listContent;
+
+  public DoTimes(List<Executable> parameterExecutables) {
+    super(parameterExecutables);
+    var = (VariableExecutable) parameterExecutables.get(0);
+    limit = (ConstantExecutable) parameterExecutables.get(1);
+    listContent = (ListExecutable) parameterExecutables.get(2);
+  }
+
+  @Override
+  public double execute(EnvironmentApi env) {
+    double val = 0; //default return
+    for (int i = 0; i < limit.execute(env); i++) {
+      for (Executable e : listContent.getList()) {
+        val = e.execute(env);
+      }
+      val ++;
+      env.getVarMap().replace(var.getSignature(), val);
+    }
+    return val;
+  }
+}
