9df64f82c63926fb115dedd5f24d506555d174d3
Kevin Deng <kd254@duke.edu>
2024-03-05 13:01:52 -0500

    Added active field to TurtleModel

--- src/main/java/slogo/model/turtle/TurtleModel.java
+++ src/main/java/slogo/model/turtle/TurtleModel.java
@@ -6,6 +6,7 @@ import slogo.model.api.TurtleRecord;
 public class TurtleModel implements TurtleModelApi {
 
   private final double id;
+  private boolean active;
   private double posX;
   private double posY;
   private double speed;
@@ -21,7 +22,12 @@ public class TurtleModel implements TurtleModelApi {
     id = ID;
     initialize();
   }
-
+  public boolean isActive(){
+    return active;
+  }
+  public void setActive(boolean activate){
+    active = activate;
+  }
   public boolean isEraseTrace(){
     return eraseTrace;
   }
@@ -93,6 +99,7 @@ public class TurtleModel implements TurtleModelApi {
   }
 
   private void initialize() {
+    this.active = false;
     this.posX = 0;
     this.posY = 0;
     this.speed = 1; //default values (might change later)
