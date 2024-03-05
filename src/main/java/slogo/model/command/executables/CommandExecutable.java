154349946238c8444eb89c141fb8f7784af3ac89
Kevin Deng <kd254@duke.edu>
2024-03-05 13:59:36 -0500

    Javadoc and cleaning for and command executable

--- src/main/java/slogo/model/command/executables/CommandExecutable.java
+++ src/main/java/slogo/model/command/executables/CommandExecutable.java
@@ -1,22 +1,14 @@
 package slogo.model.command.executables;
 
-import java.util.ArrayList;
 import java.util.List;
 import slogo.model.environment.EnvironmentApi;
 
 public abstract class CommandExecutable implements Executable {
 
-  private final List<Executable> parameterExecutables;
-  private List<Double> parameterData;
-
-  public CommandExecutable(List<Executable> parameterExecutables) {
-    this.parameterExecutables = parameterExecutables;
-    this.parameterData = new ArrayList<>();
+  private List<Executable> parameterExecutables;
+  public CommandExecutable(List<Executable> params) {
+    parameterExecutables = params;
   }
   @Override
   public abstract double execute(EnvironmentApi env);
-
-  protected List<Double> getParameterData() {
-    return parameterData;
-  }
 }
