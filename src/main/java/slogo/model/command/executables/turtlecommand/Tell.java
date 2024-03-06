7e2554f87f4201a05dc53da7b1a3a6581f11908c
Kevin Deng <kd254@duke.edu>
2024-03-06 18:29:38 -0500

    SLOGO-83 Tell command executable created

--- src/main/java/slogo/model/command/executables/turtlecommand/Tell.java
+++ src/main/java/slogo/model/command/executables/turtlecommand/Tell.java
@@ -1,2 +1,25 @@
-package slogo.model.command.executables.turtlecommand;public class Tell {
+package slogo.model.command.executables.turtlecommand;
+
+import java.util.List;
+import slogo.model.command.executables.CommandExecutable;
+import slogo.model.command.executables.Executable;
+import slogo.model.environment.EnvironmentApi;
+import slogo.model.turtle.TurtleModel;
+
+public class Tell extends CommandExecutable {
+  List<Executable> ids;
+  public Tell(List<Executable> params) {
+    super(params);
+    ids = params;
+  }
+
+  @Override
+  public double execute(EnvironmentApi env) {
+    ids.forEach(exec -> {
+      double id = exec.execute(env);
+      env.getTurtleMap().putIfAbsent(id,new TurtleModel(id));
+      env.getTurtleMap().get(id).setActive(true);
+    });
+    return 0;
+  }
 }
