aded1c22ce07c7126361fd41afb13176aab19457
Kevin Deng <kd254@duke.edu>
2024-03-06 18:30:14 -0500

    SLOGO-82 Turtles command executable created

--- src/main/java/slogo/model/command/executables/turtlecommand/Turtles.java
+++ src/main/java/slogo/model/command/executables/turtlecommand/Turtles.java
@@ -1,2 +1,22 @@
-package slogo.model.command.executables.turtlecommand;public class Turtles {
+package slogo.model.command.executables.turtlecommand;
+
+import java.util.List;
+import slogo.model.command.executables.Executable;
+import slogo.model.environment.EnvironmentApi;
+
+public class Turtles extends TurtleExecutable{
+
+  /**
+   * Constructor for Turtles command
+   *
+   * @param params parameters passed into functions.
+   */
+  public Turtles(List<Executable> params) {
+    super(params);
+  }
+
+  @Override
+  public double executeSingle(EnvironmentApi env) {
+    return env.getTurtleMap().size();
+  }
 }
