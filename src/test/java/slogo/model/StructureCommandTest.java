dd4c1c53f3a022cec3ef0163530602b11a891488
Alisha Zhang <xz352@duke.edu>
2024-03-06 17:28:10 -0500

    dotimes, repeate, for tested

--- src/test/java/slogo/model/StructureCommandTest.java
+++ src/test/java/slogo/model/StructureCommandTest.java
@@ -1,6 +1,7 @@
 package slogo.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.Test;
 import slogo.model.command.executables.ConstantExecutable;
 import slogo.model.command.executables.Executable;
 import slogo.model.command.executables.ListExecutable;
+import slogo.model.command.executables.VariableExecutable;
+import slogo.model.command.executables.structures.DoTimes;
+import slogo.model.command.executables.structures.ForCommand;
 import slogo.model.command.executables.structures.Repeat;
 import slogo.model.command.executables.turtlecommand.Forward;
 import slogo.model.environment.Environment;
@@ -33,4 +37,33 @@ public class StructureCommandTest {
     repeat.execute(env);
     assertEquals(50*5,env.getTurtleMap().get(1.0).getPosX());
   }
+
+  @Test
+  void testDoTimes(){
+    ListExecutable commandList = new ListExecutable(List.of(new Forward(List.of(new ConstantExecutable(50)))));
+    VariableExecutable var = new VariableExecutable("var");
+    assertNull(env.getVarMap().get(var.getSignature()));
+    ListExecutable varConstList = new ListExecutable(List.of(var, new ConstantExecutable(5)));
+    params.add(varConstList);
+    params.add(commandList);
+    DoTimes doTimes = new DoTimes(params);
+    doTimes.execute(env);
+    assertEquals(5,env.getVarMap().get(var.getSignature()));
+    assertEquals(50*5,env.getTurtleMap().get(1.0).getPosX());
+  }
+
+  @Test
+  void TestFor(){
+    ListExecutable commandList = new ListExecutable(List.of(new Forward(List.of(new ConstantExecutable(50)))));
+    VariableExecutable var = new VariableExecutable("var");
+    assertNull(env.getVarMap().get(var.getSignature()));
+    ListExecutable varConstList = new ListExecutable(List.of(var, new ConstantExecutable(1), new ConstantExecutable(10), new ConstantExecutable(2)));
+    params.add(varConstList);
+    params.add(commandList);
+    ForCommand forCommand = new ForCommand(params);
+    forCommand.execute(env);
+    assertEquals(9,env.getVarMap().get(var.getSignature()));
+    assertEquals(50*5,env.getTurtleMap().get(1.0).getPosX());
+  }
+
 }
