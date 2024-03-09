8d74700c1297b84ec0158160236692e7d67cf3bd
Alisha Zhang <xz352@duke.edu>
2024-03-09 00:37:33 -0500

    DESIGN-07: javadoc for mathcommand

--- src/main/java/slogo/model/command/executables/mathcommand/OrCommand.java
+++ src/main/java/slogo/model/command/executables/mathcommand/OrCommand.java
@@ -5,17 +5,33 @@ import slogo.model.command.executables.CommandExecutable;
 import slogo.model.command.executables.Executable;
 import slogo.model.environment.EnvironmentApi;
 
+/**
+ * This class represents a command executable that performs logical OR operation.
+ */
+
 public class OrCommand extends CommandExecutable {
 
   private final Executable test1;
   private final Executable test2;
 
+
+  /**
+   * Constructs a new {@code OrCommand} with the specified parameter executables.
+   *
+   * @param parameterExecutables the list of parameter executables representing the boolean expressions
+   */
   public OrCommand(List<Executable> parameterExecutables) {
     super(parameterExecutables);
     test1 = parameterExecutables.get(0);
     test2 = parameterExecutables.get(1);
   }
 
+  /**
+   * Executes the command and returns 1 if either of the boolean expressions evaluates to true, and 0 otherwise.
+   *
+   * @param env the environment in which the command is executed, including the turtle model, user defined variables/commands, and the environment dimensions
+   * @return 1 if either of the boolean expressions evaluates to true, and 0 otherwise
+   */
   @Override
   public double execute(EnvironmentApi env) {
     if (Math.abs(test1.execute(env)) > 0.001 || Math.abs(test2.execute(env)) > 0.001) {
