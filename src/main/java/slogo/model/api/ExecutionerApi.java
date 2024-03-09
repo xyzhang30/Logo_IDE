0d929d33903318c03c1cedc766481766c1a2bb1c
Alisha Zhang <xz352@duke.edu>
2024-03-09 00:11:39 -0500

    DESIGN-07: javadoc for api packag

--- src/main/java/slogo/model/api/ExecutionerApi.java
+++ src/main/java/slogo/model/api/ExecutionerApi.java
@@ -20,25 +20,75 @@ public interface ExecutionerApi {
    */
   void step();
 
+  /**
+   * Calls parser to parse the command tree from the provided input record.
+   *
+   * @param commandInput the input record containing the command tree
+   */
   void parseTree(InputRecord commandInput);
 
+  /**
+   * Runs the next instruction in the tree.
+   */
   void runNext();
 
+  /**
+   * Checks if there are more instructions in the tree to be executed.
+   * @return true if there are more instructions, false otherwise
+   */
   boolean hasNext();
 
+  /**
+   * Retrieves the turtle model associated with the specified ID.
+   *
+   * @return the turtle model
+   * @deprecated This method is deprecated and will be removed in future versions
+   */
   @Deprecated
   TurtleModelApi getTurtleModel();
 
+  /**
+   * Retrieves a map of all turtle models associated with their IDs.
+   *
+   * @return a map of turtle models
+   */
   Map<Double, TurtleModelApi> getTurtleModels();
 
+  /**
+   * Retrieves the IDs of all active turtles.
+   *
+   * @return a list of active turtle IDs
+   */
   List<Double> getActiveTurtles();
 
+  /**
+   * Retrieves the command history.
+   *
+   * @return the command history
+   */
   CommandHistory getHistory();
 
+  /**
+   * Retrieves the XML parser used for parsing commands.
+   *
+   * @return the XML parser
+   */
   CommandXmlParser getXmlParser();
 
+
+  /**
+   * Saves the current state to a file.
+   *
+   * @param fileName the name of the file
+   * @param filePath the path where the file should be saved
+   */
   void saveFile(String fileName, String filePath);
 
+  /**
+   * Retrieves the map of variables.
+   *
+   * @return a map of variables
+   */
   Map<String, Double> getVariableMap();
 //  Map<String, Executable> getFunctionMap();
 }
--- src/main/java/slogo/model/api/ModelFactory.java
+++ src/main/java/slogo/model/api/ModelFactory.java
@@ -2,7 +2,17 @@ package slogo.model.api;
 
 import slogo.model.command.Executioner;
 
+/**
+ * Abstract factory class for creating instances of different components in the SLogo model.
+ */
+
 public abstract class ModelFactory {
+
+  /**
+   * Creates and returns a new instance of the {@link ExecutionerApi} interface.
+   *
+   * @return a new instance of the {@link ExecutionerApi} interface
+   */
   public ExecutionerApi createExecutioner(){
     return new Executioner();
   }
